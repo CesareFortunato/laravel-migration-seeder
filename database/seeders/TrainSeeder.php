e3be113a4883afc6381fba681105c781efef0aaf
CesareFortunato <cesare.fortunato89@gmail.com>
2026-04-20 20:01:56 +0200

    fix:seeder

--- database/seeders/TrainSeeder.php
+++ database/seeders/TrainSeeder.php
@@ -9,44 +9,37 @@ use Faker\Generator as Faker;
 
 class TrainSeeder extends Seeder
 {
-    public function run()
+    public function run(Faker $faker): void
     {
-        $faker = \Faker\Factory::create();
-        $ritardo = $faker->numberBetween(0, 60);
+
 
         for ($i = 0; $i < 20; $i++) {
-            Train::create([
-                'azienda' => $faker->randomElement(['Trenitalia', 'Italo']),
 
-                'stazione_partenza' => $faker->city(),
-                'stazione_arrivo' => $faker->city(),
+            $newTrain = new Train();
 
-                'orario_partenza' => $faker->dateTimeBetween('-1 day', '+2 days'),
-                'orario_arrivo' => $faker->dateTimeBetween('+1 hour', '+3 days'),
-
-                'codice_treno' => strtoupper($faker->bothify('??####')),
-
-                'totale_carrozze' => $faker->numberBetween(5, 12),
-
-                'in_orario' => $ritardo === 0,
-
-                'cancellato' => $faker->boolean(10),
-
-                'ritardo' => $ritardo,
-
-                'binario' => $faker->randomElement([
-                    '1',
-                    '2',
-                    '3',
-                    '4',
-                    '5',
-                    '6',
-                    '7',
-                    '8',
-                    '3Est',
-                    '4Ovest'
-                ]),
-            ]);
+            $newTrain->azienda = $faker->azienda;
+            $newTrain->stazione_partenza = $faker->city;
+            $newTrain->stazione_arrivo = $faker->city;
+            $newTrain->orario_partenza = $faker->time;
+            $newTrain->orario_arrivo = $faker->time;
+            $newTrain->codice_treno = $faker->bothify('??####');
+            $newTrain->totale_carrozze = $faker->numberBetween(5, 12);
+            $newTrain->in_orario = $faker->boolean;
+            $newTrain->cancellato = $faker->boolean;
+            $newTrain->binario = $faker->randomElement([
+                '1',
+                '2',
+                '3',
+                '4',
+                '5',
+                '6',
+                '7',
+                '8',
+                '3Est',
+                '4Ovest'
+            ])
+            ;
+            $newTrain->save();
         }
     }
 }
