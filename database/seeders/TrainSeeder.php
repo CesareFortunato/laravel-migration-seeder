63648bbebbfaa00130b0514bec2b767e82d8ccf1
CesareFortunato <cesare.fortunato89@gmail.com>
2026-04-20 20:07:40 +0200

    fix on seed

--- database/seeders/TrainSeeder.php
+++ database/seeders/TrainSeeder.php
@@ -17,12 +17,12 @@ class TrainSeeder extends Seeder
 
             $newTrain = new Train();
 
-            $newTrain->azienda = $faker->azienda;
+            $newTrain->azienda = $faker->company;
             $newTrain->stazione_partenza = $faker->city;
             $newTrain->stazione_arrivo = $faker->city;
             $newTrain->orario_partenza = $faker->time;
             $newTrain->orario_arrivo = $faker->time;
-            $newTrain->codice_treno = $faker->bothify('??####');
+            $newTrain->codice_treno = $faker->bothify('??-####');
             $newTrain->totale_carrozze = $faker->numberBetween(5, 12);
             $newTrain->in_orario = $faker->boolean;
             $newTrain->cancellato = $faker->boolean;
